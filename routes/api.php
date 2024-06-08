12edbab67558194ac8e5f536f21d13d851711c96
AziVey <cytrsx01@gmail.com>
2024-06-08 18:09:16 +0700

    added category database for such

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,4 +10,13 @@ Route::group([
     'prefix' => 'auth'
 ], function(){
     Route::post('login', [AuthController::class, 'login'])->name('login');
+});
+
+
+Route::group([
+    'middleware' => 'api',
+], function() {
+    Route::resources([
+        CategoryController::class
+    ]);
 });
